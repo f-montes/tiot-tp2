afa694ce46d4b06c612a58cea55e43a768b39e8e
f-montes <f.montes@akribis.info>
2021-06-27 08:56:54 -0300

    Test que verifica que al agregar un valor a una clave existente, el valor se actualiza

--- test/test_lista.js
+++ test/test_lista.js
@@ -1,9 +1,9 @@
 /**
  *--En una lista vacia hay cero elementos.
- *--.En una lista vacia no se encuentra ninguna clave
+ *--En una lista vacia no se encuentra ninguna clave
  *--Cuando se agrega un elemento a una lista vacia hay un elemento.
- *-Cuando se agrega un elementos a una lista vacia se puede recuperar el valor a partir de la clave.
- *Cuando se agrega una clave que ya esta en la lista se actualiza el valor correspondiente.
+ *--Cuando se agrega un elementos a una lista vacia se puede recuperar el valor a partir de la clave.
+ *-Cuando se agrega una clave que ya esta en la lista se actualiza el valor correspondiente.
  *Cuando se agrega un elelmento a una lista vacia la lista de claves esta ordenada.
  *Cuadoo se agrega un elemento al principio la lista de claves esta ordenada.
  *Cuando se agrega un elemento al final la lista de claves esta ordenada.
@@ -35,4 +35,13 @@ describe("cuando se agrega un elemento a una lista vacía" , function() {
     it("se puede recuperar el valor a partir de la clave", function() {
         assert.equal(lista.find("clave"), "valor");
     })
+})
+
+describe("Cuando se agrega una clave que ya esta en la lista" , function() {
+    var lista = new Lista();
+    lista.add("clave", "valor2");
+    
+    it("se actualiza el valor correspondiente", function() {
+        assert.equal(lista.find("clave"), "valor2");
+    })  
 })
